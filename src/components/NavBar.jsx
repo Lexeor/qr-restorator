737374e55e3f199719061b86cc46c8faff2d95d5
Lexeor <lexeor.ross@gmail.com>
2023-07-04 13:55:26 +0400

    navbar mockup started

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const NavBar = () => {
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <div className="nav-location">
+        <div className="nav-location-title">Shakespeare</div>
+        <div className="nav-location-place">
+          <i className="ri-map-pin-2-fill"></i>
+          Akdeniz Blv. No:53
+        </div>
+      </div>
+      <div className="nav-buttons">
+        <button className="menu">
+          <i className="ri-shopping-cart-line"></i>
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
