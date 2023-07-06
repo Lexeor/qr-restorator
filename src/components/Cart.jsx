26a44019e01d0bf54eea24dd2a0625173c4ac146
Lexeor <lexeor.ross@gmail.com>
2023-07-06 11:46:22 +0400

    total price added to cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,6 +5,13 @@ import { addToCart, removeFromCart } from "../app/cartSlice";
 function Cart({ show, items }) {
   const containerClass = show ? "panel cart" : "panel cart hidden";
 
+  const totalPrice = useSelector((state) =>
+    state.cart.itemsList?.reduce((acc, item) => {
+      return acc + item.totalPrice;
+    }, 0)
+  );
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+
   const renderItems = items.map((item) => (
     <div key={item.id}>
       {item.quantity} {item.name} ${item.totalPrice}
@@ -16,6 +23,12 @@ function Cart({ show, items }) {
       <div className="content-wrapper">
         <h2>Your order</h2>
         {renderItems}
+        <hr />
+        <div className="total-row">
+          <span>Total</span>
+          <strong>${totalPrice}</strong>
+        </div>
+        <button className="btn-primary">Checkout</button>
       </div>
     </div>
   );
