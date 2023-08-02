a29da6bd40409807b413aa6c2a1ebe4f43b6221c
Lexeor <lexeor.ross@gmail.com>
2023-08-02 14:40:05 +0400

    add AutoTextSize to Subheader component

--- src/components/Subheader.jsx
+++ src/components/Subheader.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { AutoTextSize } from "auto-text-size";
 
 function Subheader({ show }) {
   const product = useSelector((state) => state.selected.item);
 
   const subheaderClass = show ? "subheader show" : "subheader";
 
-  return <div className={subheaderClass}>{product ? product.name : ""}</div>;
+  return (
+    <div className={subheaderClass}>
+      <AutoTextSize mode="oneline" minFontSizePx={10} maxFontSizePx={24}>
+        {product ? product.name : ""}
+      </AutoTextSize>
+    </div>
+  );
 }
 
 export default Subheader;
